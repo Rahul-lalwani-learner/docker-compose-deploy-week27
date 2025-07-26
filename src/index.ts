263e183cdfc18d289f13f6e383d5b29092904f6d
Rahul <rahulkrishlalwani@gmail.com>
2025-07-26 14:05:34 +0530

    Intialize the express project

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client';
+import express from 'express'
+
+const app = express(); 
+const prismaClient = new PrismaClient(); 
+
+app.get("/", async (req, res)=>{
+    await prismaClient.user.findMany({})
+    res.json({
+        "message": "get endpoint"
+    })
+})
+
+app.post("/",async (req, res)=>{
+    await prismaClient.user.create({
+        data: {
+            username: Math.random().toString(), 
+            password: Math.random().toString()
+        }
+    })
+    res.json({
+        "message": "post endpoint"
+    })
+})
+
+app.listen(3000);
